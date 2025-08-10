50d6f22b33ab7cdcf450ad6658b919ce3c313ab9
Sittichaikim <sittichaikim27@gmail.com>
2025-08-11 04:08:04 +0700

    fix a to link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,9 @@ export default function Navbar() {
   return (
     <nav className="bg-white dark:bg-emerald-600 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link href="https://www.instagram.com/kxmm_son/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TT - ติวเตอร์</span>
-        </a>
+        </Link>
 
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
@@ -41,13 +41,13 @@ export default function Navbar() {
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
             <li>
-              <a href="/" className="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0 " aria-current="page">Home</a>
+              <Link href="/" className="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0 " aria-current="page">Home</Link>
             </li>
             <li>
-              <a href="/about" className="block py-2 px-3 text-gray-900 rounded-sm md:hover:bg-transparent md:p-0  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+              <Link href="/about" className="block py-2 px-3 text-gray-900 rounded-sm md:hover:bg-transparent md:p-0  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
             </li>
             <li>
-              <a href="/tutor" className="block py-2 px-3 text-gray-900 rounded-sm md:hover:bg-transparent md:p-0  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Tutor</a>
+              <Link href="/tutor" className="block py-2 px-3 text-gray-900 rounded-sm md:hover:bg-transparent md:p-0  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Tutor</Link>
             </li>
           </ul>
         </div>
